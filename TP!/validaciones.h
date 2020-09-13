b1106c8d1adf3f30b548754293bebe79b8fcb557
maxiwilde <66798062+maxiwilde@users.noreply.github.com>
2020-09-12 23:59:47 -0300

    a

---
+++ TP!/validaciones.h
@@ -0,0 +1,23 @@
+#ifndef VALIDACIONES_H_INCLUDED
+#define VALIDACIONES_H_INCLUDED
+#endif // VALIDACIONES_H_INCLUDED
+
+
+/** \brief Validad la salida del programa
+ *
+ * \param a char
+ * \return int
+ *
+ */
+int finalizar(char a);
+
+
+ /** \brief Valida que no sea 0
+  *
+  * \param
+  * \param
+  * \return
+  *
+  */
+
+ int operador (int a);
